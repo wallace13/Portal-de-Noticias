36dcd477111b4730a4099c5ba9da0e7889c95c5e
wallace13 <wsalesg@hotmail.com>
2023-03-11 14:12:47 -0300

    Portal de Noticias Com Redis

---
+++ app/Http/Controllers/NoticiaController.php
@@ -0,0 +1,109 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Noticia;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Cache;
+
+class NoticiaController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //criar um dado dentro do bd Redis
+        //Cache::put('site', 'jorgesantana.net.br', 10);
+        //chave, valor, tempo em segundos para expirar o dado em memória
+
+        //recuperar um dado dentro do bd Redis
+        //$site = Cache::get('site');
+        //echo $site;
+
+        $noticias = [];
+        
+        /*
+        if(Cache::has('dez_primeiras_noticias')) {
+            $noticias = Cache::get('dez_primeiras_noticias');
+        } else {
+            $noticias = Noticia::orderByDesc('created_at')->limit(10)->get();
+            Cache::put('dez_primeiras_noticias', $noticias, 15);
+        }       
+        */
+        
+        $noticias = Cache::remember('dez_primeiras_noticias', 15, function() {
+            return Noticia::orderByDesc('created_at')->limit(10)->get();
+        });
+
+        return view('noticia', ['noticias' => $noticias]);
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Models\Noticia  $noticia
+     * @return \Illuminate\Http\Response
+     */
+    public function show(Noticia $noticia)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\Models\Noticia  $noticia
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(Noticia $noticia)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Models\Noticia  $noticia
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, Noticia $noticia)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Models\Noticia  $noticia
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(Noticia $noticia)
+    {
+        //
+    }
+}
---
+++ database/factories/NoticiaFactory.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace Database\Factories;
+
+use App\Models\Noticia;
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+class NoticiaFactory extends Factory
+{
+    /**
+     * The name of the factory's corresponding model.
+     *
+     * @var string
+     */
+    protected $model = Noticia::class;
+
+    /**
+     * Define the model's default state.
+     *
+     * @return array
+     */
+    public function definition()
+    {
+        return [
+            'titulo' => $this->faker->text(rand(10, 40)),
+            'noticia' => $this->faker->text(rand(200, 400))
+        ];
+    }
+}
